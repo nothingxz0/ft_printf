858b1202b55b685453fa371e309636f98a5fe446
Soufiane Lasfar <slasfar@c2r8p5.1337.ma>
2024-11-06 23:45:28 +0100

    waaaaaaa

---
+++ ft_printf.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slasfar <slasfar@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 15:05:07 by slasfar           #+#    #+#             */
+/*   Updated: 2024/11/06 23:33:10 by slasfar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *str, ...)
+{
+	int		counter;
+	int		i;
+	va_list	args;
+
+	va_start(args, str);
+	i = 0;
+	counter = 0;
+	while (str[i])
+	{
+		if (str[i] == '%' && ft_strchr("cspdiuxX\%", str[i + 1]))
+		{
+			counter += ft_ifs(str[++i], args);
+			i++;
+		}
+		else
+			counter += ft_putchar(str[i++]);
+	}
+	va_end(args);
+	return (counter);
+}
